72e1157a3636cab1fe713d7e2ef7dfad36b32def
yair <yair99@gmail.com>
2015-08-18 17:06:22 +0300

    Create play.sh
    autorun file

---
+++ nodeRestToSerial/play.sh
@@ -0,0 +1,26 @@
+SCRIPTNAME=`basename $0`
+PIDFILE=/var/run/${SCRIPTNAME}.pid
+
+
+if [ -f ${PIDFILE} ]; then
+   #verify if the process is actually still running under this pid
+   OLDPID=`cat ${PIDFILE}`
+   RESULT=`ps -ef | grep ${OLDPID} | grep ${SCRIPTNAME}`  
+
+   if [ -n "${RESULT}" ]; then
+     echo "Script already running! Exiting"
+     exit 255
+   fi
+
+fi
+
+
+#grab pid of this process and update the pid file with it
+PID=`ps -ef | grep ${SCRIPTNAME} | head -n1 |  awk ' {print $2;} '`
+echo ${PID} > ${PIDFILE}
+
+sudo /opt/node/bin/node /home/pi/ovenpi/nodeRestToSerial/index.js /dev/ttyUSB0
+
+if [ -f ${PIDFILE} ]; then
+    rm ${PIDFILE}
+fi
